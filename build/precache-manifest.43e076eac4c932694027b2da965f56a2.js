d8f3b4a4d8dfe9e84d50d90508800a9c88bbedde
shiv4ngi <come2shivangu@gmail.com>
2021-06-30 14:35:59 +0530

    Static notes and delete functionality

---
+++ build/precache-manifest.43e076eac4c932694027b2da965f56a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506e0a3346f6281b2d1cb198bd273a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "3733cc7cdda55fd05ccd",
+    "url": "/static/js/main.82ee9c46.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
